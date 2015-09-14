3b71ac48c1b87cd62fc8b1c20a938085d52ed6b1
olivertappin <olivertappin@gmail.com>
2015-09-14 16:00:10 +0100

    Added activation functionality and checked URL exists before downloading

--- wp-ppm/wp-ppm.php
+++ wp-ppm/wp-ppm.php
@@ -8,6 +8,7 @@
  */
 
 require dirname(__DIR__) . '/../../../../wp-load.php';
+require dirname(__DIR__) . '/../../../../wp-admin/includes/plugin.php';
 
 use Core\Cli;
 use Core\Environment;
@@ -18,8 +19,10 @@ use Core\Wordpress\Wordpress;
 // init: Regenerate the current plugins.json file using the files in the plugins directory
 
 $installed = 0;
+$activated = 0;
 $file = __DIR__ . '/../../plugins.json';
 $pluginsDirectory = realpath(__DIR__ . '/../../../../plugins');
+$pluginsData = get_plugins(); // Use this to skip installed plugins (with correct versions) and create the json file using `init`
 
 if (!file_exists($file)) {
     Cli::uiError('The plugins.json file does not exist. To create one, run `php plugins.php init`');
@@ -34,14 +37,30 @@ if ($data === null) {
     exit;
 }
 
-foreach ($data->plugins as $plugin => $version) {
+foreach ($data->plugins as $plugin => $data) {
     Cli::uiMessage('Installing plugin: ' . $plugin);
+
+    $activate = true; // Activate the plugin by default
+
+    if (is_object($data)) {
+        $version = ($data->version === null) ? false : $data->version;
+        $activate = ($data->activate === null) ? false : $data->activate;
+    } else {
+        $version = ($data === null) ? false : $data;
+    }
 
     $source = 'https://downloads.wordpress.org/plugin/' . $plugin . '.' . $version . '.zip';
     $target = $pluginsDirectory . '/' . $plugin . '.zip';
 
-    $rh = fopen($source, 'rb');
-    $wh = fopen($target, 'w+b');
+    $rh = @fopen($source, 'rb');
+    $wh = @fopen($target, 'w+b');
+
+    if (!$rh) {
+        Cli::uiWarning('Plugin URL does not exist for plugin: ' . $plugin);
+        Cli::uiWarning('Attemping plugin installation without version');
+        $source = 'https://downloads.wordpress.org/plugin/' . $plugin . '.zip';
+        $rh = @fopen($source, 'rb');
+    }
 
     if (!$rh || !$wh) {
         Cli::uiError('Failed to install plugin: ' . $plugin);
@@ -62,10 +81,29 @@ foreach ($data->plugins as $plugin => $version) {
     exec('cd ' . $pluginsDirectory . ' && unzip -o ' . $target);
     unlink($target);
 
+    $pluginData = get_plugins('/' . $plugin);
+    $pluginFile = key($pluginData);
+
+    $pluginName = isset($pluginData[$pluginFile]) ? $pluginData[$pluginFile] : null;
+
+    if ($activate) {
+        $result = activate_plugin($pluginsDirectory . '/' . $plugin . '/' . $pluginFile);
+        if (is_wp_error($result)) {
+            Cli::uiError('Failed to activate plugin: ' . $plugin);
+        } else {
+            $activated++;
+        }
+    }
+
     $installed++;
 }
 
 if ($installed > 0) {
     Cli::uiMessage('----------');
     Cli::uiMessage($installed . ' plugins installed successfully');
+}
+
+if ($activated > 0) {
+    Cli::uiMessage('----------');
+    Cli::uiMessage($activated . ' plugins activated successfully');
 }
